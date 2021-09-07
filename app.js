0769f6ed5539f555263251edbc3f83ef42ee8a8a
HARSH HG GARG <harshgargthapar@gmail.com>
2021-09-08 02:01:30 +0530

    Dynamic blog website travesing through blogs made easy

--- app.js
+++ app.js
@@ -3,11 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const _=require("lodash");
 const homeStartingContent = "Hello my first blog website ";
-// const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-// const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const aboutContent = "i am a web developer ";
+const contactContent="Contact me through mail";
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,10 +17,55 @@ app.use(express.static("public"));
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+let posts=[];
 
 app.get("/",function(req,res){
-    res.render("home",{startingContent:homeStartingContent});
+    res.render("home",{
+        startingContent:homeStartingContent,
+        posts:posts
+    });
+    // console.log(posts);
 });
+app.get("/about",function(req,res){
+    res.render("about",{aboutcontent:aboutContent});
+});
+app.get("/contact",function(req,res){
+    res.render("contact",{contactcontent:contactContent});
+});
+app.get("/compose",function(req,res){
+    res.render("compose");
+});
+app.post("/compose",function(req,res){
+    const post={
+        title:req.body.postTitle,
+        content:req.body.postBody
+    };
+    posts.push(post);
+    res.redirect("/");
+});
+
+app.get("/posts/:postName",function(req,res){
+    const reqtitle=_.lowerCase(req.params.postName);
+    posts.forEach(function(post){
+        const stredfile=_.lowerCase(post.title);
+        if(stredfile===reqtitle)
+        {
+        res.render("post",{
+           title:post.title,
+           content:post.content 
+        });
+
+        }
+    })
+       
+    
+});
+// app.get("/",function(req,res){
+//     res.render("about",{aboutContent:aboutContent});
+// });
+// app.get("/",function(req,res){
+//     res.render("contact",{contactContent:contactContent});
+// });
 
 
     
