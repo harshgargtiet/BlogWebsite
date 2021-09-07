e2599df4967e1b6aaf5e2af5a51bed03b4f43e8e
HARSH HG GARG <harshgargthapar@gmail.com>
2021-09-07 17:26:05 +0530

    using ejs in node express

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
-const express=require("express");
-const bodyParser= require("body-parser");
+//jshint esversion:6
 
-const app=express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const homeStartingContent = "Hello my first blog website ";
+// const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+// const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
 
 app.get("/",function(req,res){
-res.sendFile(__dirname+"/index.html")
+    res.render("home",{startingContent:homeStartingContent});
 });
 
-app.get("/about",function(req,res){
-    res.send("<h1> created with love by Harsh </h1>")
-    });
-    app.get("/contact",function(req,res){
-        res.send("<h1> Harsh Garg 7009115485 </h1>")
-        });
 
-app.post("/",function(req,res){
-    var blogname =req.body.blogName;
-    res.send("Your Blog Name is  " + blogname);
+    
+    
+// app.get("/",function(req,res){
+// res.sendFile(__dirname+"/index.html")
+// });
 
-});
-app.listen(3000,function(){
-    console.log("serven up and running on port 3000")
-});
+// app.get("/about",function(req,res){
+//     res.send("<h1> created with love by Harsh </h1>")
+//     });
+//     app.get("/contact",function(req,res){
+//         res.send("<h1> Harsh Garg 7009115485 </h1>")
+//         });
+
+// app.post("/",function(req,res){
+//     var blogname =req.body.blogName;
+//     res.send("Your Blog Name is  " + blogname);
+
+// });
+// app.listen(3000,function(){
+//     console.log("serven up and running on port 3000")
+// });
